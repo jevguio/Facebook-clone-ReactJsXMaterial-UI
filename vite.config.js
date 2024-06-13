0ecdb9badf0a0cca0aeaf49d047cfab329bb070a
jevguio <jevguio@gmail.com>
2024-06-13 12:28:47 +0800

    Merge remote-tracking branch 'origin/main'

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Configure Vite to use a custom server setup
-    middlewareMode: 'express',
-    setupMiddleware: (app) => {
-      // Example middleware to set Permissions-Policy header
-      app.use((req, res, next) => {
-        res.setHeader('Permissions-Policy', 'geolocation=(self "https://jevguio.github.io/")');
-        next();
-      });
-    },
-  },
+  build: {
+    outDir: 'build', // Ensure this matches with your deployment script expectations
+  }, 
 })
